bf4285e359187175f672142e75e898e42b2250ce
adugeen <konstantzts@gmail.com>
2020-02-24 22:56:51 +0500

    Добавление остальных файлов

---
+++ FormatTransformerLib/RuleManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormatTransformerLib
+{
+    class RuleManager
+    {
+        public void AddRule()
+        {
+
+        }
+
+        public void DeleteRule()
+        {
+
+        }
+
+        public void RenameRule()
+        {
+
+        }
+
+        public void GetRule()
+        {
+
+        }
+    }
+}
---
+++ FormatTransformerLib/Transformer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+namespace FormatTransformerLib
+{
+    public class Transformer
+    {
+        private string file;
+        private string rule;
+        private string result;
+
+        public void AddFile(string file)
+        {
+            this.file = file;
+        }
+
+        public void AddRule(string rule)
+        {
+            this.rule = rule;
+        }
+
+        public bool CheckFileRule()
+        {
+            return false;
+        }
+
+        public void Transform()
+        {
+            XslTransform xslt = new XslTransform();
+            xslt.Load(rule);
+            xslt.Transform(file, result);
+        }
+
+        public object GetResult()
+        {
+            return result;
+        }
+    }
+}
